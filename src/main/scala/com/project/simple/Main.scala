9612dba93dae79dcb08462afa5235f7844fc49a1
Brian Jing <brianjing404@gmail.com>
2021-09-29 16:28:41 +1000

    Add a basic test

--- src/main/scala/com/project/simple/Main.scala
+++ src/main/scala/com/project/simple/Main.scala
@@ -5,6 +5,8 @@ import cats.effect.{ExitCode, IO, IOApp}
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] =
     for {
-      _ <- IO { println("Hello World!") }
+      _ <- IO { println(helloWorld) }
     } yield ExitCode.Success
+
+  def helloWorld: String = "Hello World!"
 }
